81c75ff10386a97acdfa807d8acd1479cb557a2c
TNHiep <hiep140701@gmail.com>
2022-12-26 17:00:12 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,63 @@
 const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
+var count = 0
+const tabsList = $$('#slide')
+const tabsControll = $$('.slide_content_tab_item')
 
+//click run slide
 function handleClick(){
-    alert('oke')
+    count++
+
+    if(count>2){
+        count = 0
+        console.log(tabsList[count])
+        console.log(tabsControll[count])
+        $('#slide.active').classList.remove('active');
+        $('.slide_content_tab_item.active').classList.remove('active');
+        tabsList[count].classList.add('active')
+        tabsControll[count].classList.add('active')
+    }else{
+        console.log(tabsList[count])
+        console.log(tabsControll[count])
+        $('#slide.active').classList.remove('active');
+        $('.slide_content_tab_item.active').classList.remove('active');
+        tabsList[count].classList.add('active')
+        tabsControll[count].classList.add('active')
+    }  
+
 }
 
-const btnNextSlide = $('.btn_next_slide')
-btnNextSlide.onclick = function(){
-    // handleClick()
-    alert('oke')
+const btnSlide = $('.btn_next_slide')
+btnSlide.onclick = () => handleClick()
+
+//Auto run slide
+setInterval(() => {
+    if (count >= 2){
+        count = 0
+        $('#slide.active').classList.remove('active');
+        $('.slide_content_tab_item.active').classList.remove('active');
+        tabsList[count].classList.add('active')
+        tabsControll[count].classList.add('active')
+    }else{
+        count++
+        $('#slide.active').classList.remove('active');
+        $('.slide_content_tab_item.active').classList.remove('active');
+        tabsList[count].classList.add('active')
+        tabsControll[count].classList.add('active')
+    }
+    console.log(count)
+}, 5000)
+
+
+//form login
+const formModal = $('#modal')
+const iconCLose = $('.close')
+const iconLogin = $('.header_item.login')
+
+iconLogin.onclick = () => {
+    formModal.classList.add('active')
 }
 
-// console.log(btnNextSlide)
-
-// var count = 0
-// const tabSlide = $$('#slide')
-// console.log(tabSlide)
-// tabSlide.forEach((slide,index) => {
-//     console.log(slide)
-//     console.log(index)
-// })
+iconCLose.onclick = () => {
+    formModal.classList.remove('active')
+}
