818909bf5ef96237e2ffbd47029d0e46b9464a80
TNHiep <hiep140701@gmail.com>
2022-12-27 13:39:50 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,15 +10,11 @@ function handleClick(){
 
     if(count>2){
         count = 0
-        console.log(tabsList[count])
-        console.log(tabsControll[count])
         $('#slide.active').classList.remove('active');
         $('.slide_content_tab_item.active').classList.remove('active');
         tabsList[count].classList.add('active')
         tabsControll[count].classList.add('active')
     }else{
-        console.log(tabsList[count])
-        console.log(tabsControll[count])
         $('#slide.active').classList.remove('active');
         $('.slide_content_tab_item.active').classList.remove('active');
         tabsList[count].classList.add('active')
@@ -45,19 +41,71 @@ setInterval(() => {
         tabsList[count].classList.add('active')
         tabsControll[count].classList.add('active')
     }
-    console.log(count)
 }, 5000)
 
 
 //form login
 const formModal = $('#modal')
-const iconCLose = $('.close')
+const iconCLose = $$('.close')
 const iconLogin = $('.header_item.login')
+const login = $('.form_login .method_default .basic')
+const formLogin = $('.form_login')
+const formInputLogin = $('.form_input_login')
+const back = $('.form_input_login .back')
+const viewPass = $('.on_off_pass')
+const inputPass = $('.input_password .password')
+const btnGoToTop = $('.btn_go_to_top')
 
+//show form login
 iconLogin.onclick = () => {
     formModal.classList.add('active')
+    formLogin.classList.add('active')
 }
 
-iconCLose.onclick = () => {
-    formModal.classList.remove('active')
+//close form login
+iconCLose.forEach( close => {
+    close.onclick = () => {
+        formModal.classList.remove('active')
+        if(formLogin.classList.contains('active')){
+            formLogin.classList.remove('active')
+        }else if(formInputLogin.classList.contains('active')){
+            formInputLogin.classList.remove('active')
+        }
+    
+    }
+})
+
+//open form input login
+login.onclick = () => {
+    formLogin.classList.remove('active')
+    formInputLogin.classList.add('active')
+}
+
+//back to option login
+back.onclick = () => {
+    formLogin.classList.add('active')
+    formInputLogin.classList.remove('active')
+}
+
+// on off pass
+viewPass.onclick = () => {
+    if(inputPass.type==='password'){
+        inputPass.type = 'text'
+    }else if(inputPass.type==='text'){
+        inputPass.type = 'password'
+    }
+}
+
+
+// Go to top
+window.onscroll = () => {
+    if(this.scrollY>500){
+        btnGoToTop.style.display = 'block'
+    }else{
+        btnGoToTop.style.display = 'none'
+    }
+}
+
+btnGoToTop.onclick = () => {
+    document.documentElement.scrollTop = 0;
 }
